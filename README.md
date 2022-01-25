61dd70f642bf3d2be3a79a8365cd9890d2c4ab11
Will Greenberg <greenberg.will@gmail.com>
2022-01-25 10:24:04 -0800

    add UI, html form, add ticket constructor, totalcost object
    Co-authored-by: Hayley M <hayleymcvay8@gmail.com>

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Describe : Movies()
 Test: The function should create an object called Movies.
 Code: Movies = { this.movie = {}; }
 Expected Outcome: ListOfPlaces = {movie}
+
+Describe : Movies.prototype.addMovie()
+
+Test: This function will add a movie to Movies
+Code: Movies.prototype.addMovie("Star Wars")
+Expected Outcome: "Movies.prototype.addMovie()
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,42 @@
 // Business
 
-function Movies() {
-  this.movie = {};
+function TotalCost() {
+  this.tickets = {};
   this.currentId = 0;
 }
 
-// Movies logic
+Cost.prototype.addTicket = function(ticket){
+  ticket.id = this.assignId();
+  this.tickets[ticket.id] = ticket
+}
+
+TotalCost.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+
+// Movie logic
+
+function Ticket(title, showTime, ticketType) {
+  this.title = title;
+  this.showTime = showTime;
+  this.ticketType = ticketType;
+}
+
+
+// Ticket.prototype.getPrice = function() {
+//   // return this.firstName + " " + this.lastName; calculation to determine the price
+// };
 
 // UI
+let totalCost = new TotalCost();
+
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
+
+
+    
+  })
+})
